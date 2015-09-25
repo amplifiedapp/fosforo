11b1d214addd3e59a2b575c3041d6c267312a372
Rafael Chiti <rafaelchiti@gmail.com>
2015-09-25 20:31:15 +0200

    Adding some basic styles and the prospects balls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {Spring} from "react-motion";
 
-const App = (props) =>
-  <h1>Catch your Match!</h1>
+const prospects = [1, 2];
+
+class Prospect extends React.Component {
+  render () {
+    return <div className="prospect"></div>
+  }
+}
+class App extends React.Component {
+  render () {
+    return <div className="prospectList">{prospects.map((prospect, i) => <Prospect key={i} />)}</div>;
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('content'));
