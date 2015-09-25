54de77589d3001708959d472adff2b4f82e9a3d0
Rafael Chiti <rafaelchiti@gmail.com>
2015-09-25 21:19:28 +0200

    Including react-motion and addin ga really bad example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Spring} from "react-motion";
+import {Spring, presets as motionPresets} from "react-motion";
 
-const prospects = [1, 2];
+
+const prospects = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10];
 
 class Prospect extends React.Component {
+  constructor () {
+    super();
+    this.state = {open: false};
+  }
+
   render () {
-    return <div className="prospect"></div>
+    let left = this.props.index * 40;
+    let top = this.props.index * 40;
+
+    return (
+      <Spring defaultValue={{size: {val: 40}}} endValue={{size: {val: this.state.open ? 100 : 40, config: motionPresets.wobbly}}}>
+        {interpolated =>
+          <div className="prospect" onClick={() => this.setState({open: !this.state.open})} style={{
+            width: `${interpolated.size.val}px`,
+            height: `${interpolated.size.val}px`,
+            left: left,
+            top: top
+          }} />
+        }
+      </Spring>
+    );
+
   }
 }
+
+class ProspectList extends React.Component {
+  render () {
+    return (
+      <div className="prospectList">
+        {prospects.map((prospect, i) => <Prospect key={i} index={i}/>)}
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render () {
-    return <div className="prospectList">{prospects.map((prospect, i) => <Prospect key={i} />)}</div>;
+    return <ProspectList />;
   }
 }
 
